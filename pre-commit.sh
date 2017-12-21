72b9e929851d6b644705b6489c51cbab4560c907
Shawn Duncan <sduncan@digitalpulp.com>
2017-12-21 14:00:20 -0500

    Add a loop to be sure that the source code is mounted

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
-
+echo "Wait for the code to mount "
+until [ -r .pre-commit-config.yaml ]
+do
+    echo "."
+    sleep 2
+done
+echo ".pre-commit-config.yaml file is present"
 if [ -n ${CI_COMMIT_ID} ]; then
   git diff-tree --no-commit-id --name-only -r ${CI_COMMIT_ID} | xargs pre-commit run --files
   EXIT_CODE=$?
