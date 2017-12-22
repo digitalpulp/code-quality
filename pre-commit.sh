4f93daa443529fb4c31ab56790bfeef1a8af65ca
Shawn Duncan <sduncan@digitalpulp.com>
2017-12-22 10:45:17 -0500

    Add a counter to escape the loop after 2 minutes.

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,9 +1,15 @@
 #! /bin/sh
 echo "Wait for the code to mount "
+COUNT=0
 until [ -r .pre-commit-config.yaml ]
 do
     echo "."
     sleep 2
+    COUNT=`expr ${COUNT} + 1`
+    if [ $COUNT -gt 60 ]; then
+        echo "Time expired waiting for config file to be mounted"
+        exit 1
+    fi
 done
 echo ".pre-commit-config.yaml file is present"
 if [ -n ${CI_COMMIT_ID} ]; then
